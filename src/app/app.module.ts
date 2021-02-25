4beec7e1280aae5cf48fc38ae984f35909a9fbea
jainayushee3 <Ayushee.Jain@lntinfotech.com>
2021-02-25 10:57:40 +0530

    'updated'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GallerytwoComponent } from './gallerytwo/gallerytwo.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    MenuComponent,
+    HomeComponent,
+    GallerytwoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gallerytwo/gallerytwo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallerytwo',
+  templateUrl: './gallerytwo.component.html',
+  styleUrls: ['./gallerytwo.component.css']
+})
+export class GallerytwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
